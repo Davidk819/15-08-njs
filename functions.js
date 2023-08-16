25f1f645d73bfff27e3eed846d8934163ef43c61
david kurman <dividiak@gmail.com>
2023-08-16 16:41:11 +0300

    targil z

--- functions.js
+++ functions.js
@@ -1,10 +1,46 @@
-module.exports = allUsers
+// module.exports = myFunction;
+// const jsonfile = require("jsonfile");
+// function allUsers(data) {
+//   let resolute = "";
+//   for (let x of data) {
+//     resolute += x.id + " " + x.email + " " + x.password + "\n";
+//   }
+//   return resolute;
+// }
 
-function allUsers(data) {
-    let resolute = ''
-    for (let x of data) {
-        resolute += x.id + " " + x.email + " " + x.password + "\n";
-    }
-    return resolute
-}    
+// const readAndWrite = async function (file, newData) {
+//   jsonfile.readFile(file, (data) => {
+//     for (const user of data) {
+//       if (user.email === newData.email) {
+//         return res.send("the email address already in use");
+//       }
+//     }
+//     if (!validator.validate(newData.email)) {
+//       return res.send("email not valid");
+//     }
+//     data.push(newData);
+//     jsonfile.writeFile(file, data);
+//   });
+// };
 
+// app.post("/",async (req, res) => {
+//     let newUser = req.body;
+//     jsonfile.readFile('data.json')
+//       .then((users) => {
+  
+//       })
+//     for (const user of users) {
+//       if (user.email === newUser.email) {
+//         return res.send("the email address already in use");
+//       }
+//     }
+//     if(!validator.validate(newUser.email)){
+//       return res.send('email not valid')
+//     }
+//     const newPassword = await bcrypt.hash(req.body.password,8);
+//     newUser.id = uuidv4();
+//     newUser.password = newPassword
+//     users.push(newUser);
+//     res.send("user save");
+//   });
+  
--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const bodyParser = require("body-parser");
 const { v4: uuidv4 } = require("uuid");
 const bcrypt = require("bcrypt");
 const validator = require("email-validator");
-const jsonfile = require('jsonfile');
+const jsonfile = require("jsonfile");
 
-const allUsers = require("./functions");
+const myFunction = require("./functions");
 
 const users = [
   { id: 1, email: "user1@example.com", password: "password123" },
@@ -14,51 +14,63 @@ const users = [
   { id: 3, email: "user3@example.com", password: "12345" },
 ];
 
-jsonfile.writeFile('data.json',[users],err => {if(err){console.log(err)} })
+jsonfile.writeFile("data.json", [users], (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
 
 app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
 });
 
-
 app.use(bodyParser.json());
+
 app.get("/:id", (req, res) => {
   let myId = req.params.id;
-  const user = users.findIndex((user) => user.id == myId)
-  let user2 = users[user]
+  const user = users.findIndex((user) => user.id == myId);
+  let user2 = users[user];
   res.json(user2);
 });
 app.get("/", (req, res) => {
-  res.json(users)
+  jsonfile.readFile('data.json').then((data) => {
+    res.json(data);
+  })
+
 });
 
-app.post("/",async (req, res) => {
+app.post("/", async (req, res) => {
   let newUser = req.body;
-  for (const user of users) {
-    if (user.email === newUser.email) {
-      return res.send("the email address already in use");
+  jsonfile.readFile("data.json").then(async (users) => {
+    for (const user of users) {
+      if (user.email === newUser.email) {
+        return res.send("the email address already in use");
+      }
     }
-  }
-  if(!validator.validate(newUser.email)){
-    return res.send('email not valid')
-  }
-  const newPassword = await bcrypt.hash(req.body.password,8);
-  newUser.id = uuidv4();
-  newUser.password = newPassword
-  users.push(newUser);
+    if (!validator.validate(newUser.email)) {
+      return res.send("email not valid");
+    }
+    const newPassword = await bcrypt.hash(req.body.password, 8);
+    newUser.id = uuidv4();
+    newUser.password = newPassword;
+    users.push(newUser);
+    jsonfile.writeFile("data.json", users);
+  });
+
   res.send("user save");
 });
 
 app.put("/:id", (req, res) => {
   let myId = req.body.id;
   const user = users.find((user) => user.id === myId);
-  const newEmail = user.find((user) => user.email === req.body.email && user.id !== myId)
-  if(!newEmail){
+  const newEmail = user.find(
+    (user) => user.email === req.body.email && user.id !== myId
+  );
+  if (!newEmail) {
     users[user] = req.body;
     return res.send("change save");
   }
-  res.send("the email address already in use")
-
+  res.send("the email address already in use");
 });
 app.delete("/:id", (req, res) => {
   let myId = req.params.id;
@@ -66,13 +78,15 @@ app.delete("/:id", (req, res) => {
   users.splice(user, 1);
   res.send("user delete");
 });
-app.post("/check",async (req, res) => {
-  let newUser = req.body
-  let user = users.find((user) => user.email === newUser.email)
-  if(!user) {return res.send("the email address not exist")}
-  const match = await bcrypt.compare(newUser.password,user.password)
-  if(match){
-    return res.send('user exist')
+app.post("/check", async (req, res) => {
+  let newUser = req.body;
+  let user = users.find((user) => user.email === newUser.email);
+  if (!user) {
+    return res.send("the email address not exist");
+  }
+  const match = await bcrypt.compare(newUser.password, user.password);
+  if (match) {
+    return res.send("user exist");
   }
   res.send("not exist");
 });
